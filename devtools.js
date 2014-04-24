c760020b8e26cbc725a9ce383f6a20671060a9f4
Rhenium <rhenium@rhe.jp>
2014-04-24 20:02:39 +0900

    装備変更時おかしくなるのを修正

--- devtools.js
+++ devtools.js
@@ -1,107 +1,114 @@
-var stypes = JSON.parse(localStorage["stypes"] || null);
-var ships = JSON.parse(localStorage["ships"] || null);
-var slotitems = JSON.parse(localStorage["slotitems"] || null);
+var send_data = function() {
+    chrome.extension.sendRequest({
+        decks: mydata.decks,
+    });
+};
 
-var my_slotitems = {};
+var master = {
+    stypes: JSON.parse(localStorage["master.stypes"] || "{}"),
+    ships: JSON.parse(localStorage["master.ships"] || "{}"),
+    slotitems: JSON.parse(localStorage["master.slotitems"] || "{}"),
+};
+
+var mydata = {
+    decks: [],
+    ships: {},
+    slotitems: {},
+    update_decks: function(decks) {
+        mydata.decks = decks.map(function(deck) {
+            return {
+                name: deck.api_name,
+                ships: deck.api_ship.map(function(id) { return mydata.ships[id]; })
+            };
+        });
+    },
+    update_ships: function(ships) {
+        if (ships.length != 1) mydata.ships = {};
+
+        ships.forEach(function(ship) {
+            mydata.ships[ship.api_id] = {
+                cond: ship.api_cond,
+                lv: ship.api_lv,
+                next_exp: ship.api_exp[1], // api_exp: [total_exp, for_next, next_level]
+                hp: [ship.api_nowhp, ship.api_maxhp],
+                slotitems: ship.api_slot.filter(function(id) { return id >= 0; }).map(function(id) {
+                    return mydata.slotitems[id].name; }),
+                name: master.ships[ship.api_ship_id].name,
+                type: master.stypes[master.ships[ship.api_ship_id].stype_id],
+            };
+        });
+    },
+    update_slotitems: function(slotitems) {
+        mydata.slotitems = {};
+        slotitems.forEach(function(slotitem) {
+            mydata.slotitems[slotitem.api_id] = {
+                name: master.slotitems[slotitem.api_slotitem_id],
+                type: null,
+            };
+        });
+    },
+};
 
 var fms = [
     {
         match: /kcsapi\/api_start2$/,
         callback: function(json, request) {
             // ships (master)
-            ships = [];
+            master.ships = [];
             json.api_data.api_mst_ship.forEach(function(ship) {
-                ships[ship.api_id] = {
-                    "name": ship.api_name,
-                    "stype_id": ship.api_stype,
+                master.ships[ship.api_id] = {
+                    name: ship.api_name,
+                    stype_id: ship.api_stype,
                 };
             });
-            localStorage["ships"] = JSON.stringify(ships);
+            localStorage["master.ships"] = JSON.stringify(master.ships);
 
             // ship types (master)
-            stypes = [];
+            master.stypes = [];
             json.api_data.api_mst_stype.forEach(function(stype) {
-                stypes[stype.api_id] = stype.api_name;
+                master.stypes[stype.api_id] = stype.api_name;
             });
-            localStorage["stypes"] = JSON.stringify(stypes);
+            localStorage["master.stypes"] = JSON.stringify(master.stypes);
 
             // slotitems (master)
-            slotitems = [];
+            master.slotitems = [];
             json.api_data.api_mst_slotitem.forEach(function(slotitem) {
-                slotitems[slotitem.api_id] = slotitem.api_name;
+                master.slotitems[slotitem.api_id] = slotitem.api_name;
             });
-            localStorage["slotitems"] = JSON.stringify(slotitems);
+            localStorage["master.slotitems"] = JSON.stringify(master.slotitems);
         }
     },
     {
         match: /kcsapi\/api_get_member\/slot_item$/,
         callback: function(json, request) {
-            my_slotitems = {};
-            json.api_data.forEach(function(slotitem) {
-                my_slotitems[slotitem.api_id] = slotitems[slotitem.api_slotitem_id];
-            });
+            mydata.update_slotitems(json.api_data);
         }
     },
     {
         match: /kcsapi\/api_port\/port$/,
         callback: function(json, request) {
-            if (!stypes || !ships || !slotitems) return;
+            mydata.update_ships(json.api_data.api_ship);
+            mydata.update_decks(json.api_data.api_deck_port);
 
-            var my_ships_data = json.api_data.api_ship;
-            var my_deck_data = json.api_data.api_deck_port;
+            send_data();
+        }
+    },
+    {
+        match: /kcsapi\/api_get_member\/ship2$/,
+        callback: function(json, request) {
+            mydata.update_ships(json.api_data.api_data);
+            mydata.update_decks(json.api_data.api_data_deck);
 
-            var my_ships = {};
-            my_ships_data.forEach(function(ship) {
-                my_ships[ship.api_id] = {
-                    "cond": ship.api_cond,
-                    "lv": ship.api_lv,
-                    "next_exp": ship.api_exp[1], // api_exp: [total_exp, for_next, next_level]
-                    "hp": [ship.api_nowhp, ship.api_maxhp],
-                    "slotitems": ship.api_slot.map(function(item_id) { return my_slotitems[item_id]; }),
-                    "name": ships[ship.api_ship_id].name,
-                    "type": stypes[ships[ship.api_ship_id].stype_id],
-                };
-            });
-
-            var data = {
-                "decks": my_deck_data.map(function(deck) { return {
-                    "name": deck.api_name,
-                    "ships": deck.api_ship.map(function(id) { return my_ships[id]; })
-                };})
-            };
-            
-            chrome.extension.sendRequest(data);
+            send_data();
         }
     },
     {
         match: /kcsapi\/api_get_member\/ship3$/,
         callback: function(json, request) {
-            if (!stypes || !ships || !slotitems) return;
+            mydata.update_ships(json.api_data.api_ship_data);
+            mydata.update_decks(json.api_data.api_deck_data);
 
-            var my_ships_data = json.api_data.api_ship_data;
-            var my_decks_data = json.api_data.api_deck_data;
-
-            var my_ships = {};
-            my_ships_data.forEach(function(ship) {
-                my_ships[ship.api_id] = {
-                    "cond": ship.api_cond,
-                    "lv": ship.api_lv,
-                    "next_exp": ship.api_exp[1], // api_exp: [total_exp, for_next, next_level]
-                    "hp": [ship.api_nowhp, ship.api_maxhp],
-                    "slotitems": ship.api_slot.map(function(item_id) { return my_slotitems[item_id]; }),
-                    "name": ships[ship.api_ship_id].name,
-                    "type": stypes[ships[ship.api_ship_id].stype_id],
-                };
-            });
-
-            var data = {
-                "decks": my_decks_data.map(function(deck) { return {
-                    "name": deck.api_name,
-                    "ships": deck.api_ship.map(function(id) { return my_ships[id]; })
-                };})
-            };
-
-            chrome.extension.sendRequest(data);
+            send_data();
         }
     },
 ];
